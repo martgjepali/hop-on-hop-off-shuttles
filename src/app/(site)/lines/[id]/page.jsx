910c719af160e77505a0da81a1e930db2c324145
Mart Gjepali <gjepalimart@gmail.com>
2025-07-25 12:07:54 +0200

    update

--- src/app/(site)/lines/[id]/page.jsx
+++ src/app/(site)/lines/[id]/page.jsx
@@ -15,11 +15,11 @@ export async function generateMetadata(props) {
     return {
       title:
         line.Name === "Culture Line"
-          ? "Gjirokaster Tour & Blue Eye Shuttle Tour – Culture Line | KMG Shuttles"
+          ? "Gjirokaster tour & Blue Eye Shuttle tour – Culture Line tour | KMG Shuttles"
           : line.Name === "Sea Line"
-          ? "Albanian Riviera Shuttle – Sea Line | KMG Shuttles"
+          ? "Albanian Riviera Shuttle tour – Sea Line tour | KMG Shuttles"
           : line.Name === "Sun Line"
-          ? "Blue Eye Shuttle Tour – Sun Line | Tours From Saranda to Blue Eye by KMG"
+          ? "Blue Eye Shuttle tour – Sun Line tour | Tours From Saranda to Blue Eye by KMG"
           : `${line.Name} Shuttle Line | KMG Shuttles`,
       openGraph: {
         title: `${line.Name} – Shuttle Line | KMG`,
@@ -27,7 +27,7 @@ export async function generateMetadata(props) {
           line.Name === "Culture Line"
             ? "Join our Culture Line shuttle tour to Gjirokaster, Blue Eye, Lekuresi Castle, and Mesopotam Monastery. Departing from Saranda daily."
             : line.Name === "Sea Line"
-            ? "Take the Albanian Riviera Shuttle – Sea Line, visiting Porto Palermo, Himare, and Borsh from Saranda. Scenic beach experience guaranteed!"
+            ? "Take the Albanian Riviera Shuttle tour – Sea Line, visiting Porto Palermo, Himare, and Borsh from Saranda. Scenic beach experience guaranteed!"
             : line.Name === "Sun Line"
             ? "Hop on the Sun Line for direct shuttles from Saranda to the Blue Eye. Fast, convenient, and perfect for day trips."
             : line.ShortDescription ||
